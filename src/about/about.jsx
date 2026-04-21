269d6871c6ce2571cfe22b534eb246a2152b7b96
WaltOlivia <livwalton1@gmail.com>
2026-04-20 19:47:31 -0600

    about fix

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,32 +2,48 @@ import React from 'react';
 import './about.css';
 
 export function About(props) {
-  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+  const [imageUrl, setImageUrl] = React.useState(
+    'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs='
+  );
   const [quote, setQuote] = React.useState('Loading...');
   const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
-  // We only want this to render the first time the component is created and so we provide an empty dependency list.
   React.useEffect(() => {
     const random = Math.floor(Math.random() * 1000);
+
     fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
       .then((response) => response.json())
       .then((data) => {
         const containerEl = document.querySelector('#picture');
 
+        if (!containerEl) return;
+
         const width = containerEl.offsetWidth;
         const height = containerEl.offsetHeight;
+
         const apiUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
         setImageUrl(apiUrl);
       })
-      .catch();
+      .catch((err) => {
+        console.error('Image fetch failed:', err);
+      });
 
     fetch('https://quote.cs260.click')
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
         setQuote(data.quote);
         setQuoteAuthor(data.author);
       })
-      .catch();
+      .catch((err) => {
+        console.error('Quote fetch failed:', err);
+        setQuote('Quote service unavailable');
+        setQuoteAuthor('');
+      });
   }, []);
 
   return (
@@ -37,11 +53,12 @@ export function About(props) {
           <img src={imageUrl} alt='stock background' />
         </div>
 
-        <p>Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake. The longer the sequence you repeat, the greater your score.</p>
+        <p>
+          Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake. The longer the sequence you repeat, the greater your score.
+        </p>
 
         <p>
-          The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for non-profit educational use only. No part of this code or application may be used outside
-          of that definition.
+          The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for non-profit educational use only. No part of this code or application may be used outside of that definition.
         </p>
 
         <div className='quote-box bg-light text-dark'>
